2de31baddb35c0973ffddcc98212902051085c8a
oneSitDown <oneSitDown>
2019-02-26 23:36:43 +0800

    更改批处理文件

--- tnt/run.bat
+++ tnt/run.bat
@@ -3,4 +3,7 @@ python xls_deploy_tool.py %%~nf xls/%%~nf.xls
 protoc tnt_deploy_%%~nf.proto --descriptor_set_out=%%~nf.protodesc
 ProtoGen\protogen -i:%%~nf.protodesc -o:%%~nf.cs
 )
+for /f "delims==" %%a in ('dir /b /s *.cs')do move /-y "%%a" cs
+for /f "delims==" %%a in ('dir /b /s *.data')do move /-y "%%a" data
+for /f "delims==" %%a in ('dir /b /s *.proto')do move /-y "%%a" proto
 pause
